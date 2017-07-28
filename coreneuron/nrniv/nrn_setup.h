3ee2aacf3cd06d5d10a7a05b4746170c053891fa
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2017-07-28 12:01:20 +0200

    fixed broken ring test

--- coreneuron/nrniv/nrn_setup.h
+++ coreneuron/nrniv/nrn_setup.h
@@ -32,7 +32,6 @@ THE POSSIBILITY OF SUCH DAMAGE.
 #include <string>
 #include "coreneuron/nrnoc/multicore.h"
 #include "coreneuron/nrniv/nrn_datareader.h"
-#include "coreneuron/utils/sdprintf.h"
 
 static int ngroup_w;
 static int* gidgroups_w;
