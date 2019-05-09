31276146368cb5e0459228e506d79bea2ee41b8e
Francesco Cremonesi <francesco.cremonesi@epfl.ch>
2019-05-09 14:34:35 +0200

    fix syntax error and undo stupid mistake

--- apps/coreneuron.cpp
+++ apps/coreneuron.cpp
@@ -33,5 +33,5 @@ int main(int argc, char** argv) {
     coreneuron::Instrumentor::init_profile();
     auto solve_core_result = solve_core(argc, argv);
     coreneuron::Instrumentor::finalize_profile();
-    return solve_core_result
+    return solve_core_result;
 }
