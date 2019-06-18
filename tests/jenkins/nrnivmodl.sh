8908506f1482c1f572ad221d6db844fbf7d5164c
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-06-18 15:49:01 +0200

    Added check for finding a compatible neuron version

--- tests/jenkins/nrnivmodl.sh
+++ tests/jenkins/nrnivmodl.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/bash
 
 set -e
-set +x
+set -x
 
 TEST_DIR="$1"
 
 . /gpfs/bbp.cscs.ch/apps/hpc/jenkins/config/modules.sh
 module load intel
-module load `module av neuron 2>&1 | grep -o -m 1 'neuron.*/parallel' | awk -F' ' '{print $1}'`
+
+neuron_version=$(module av neuron 2>&1 | grep -o -m 1 '^neuron.*/parallel$' | awk -F' ' '{print $1}')
+if [[ $neuron_version ]]; then
+    module load $neuron_version
+else
+    echo "No compatible neuron version found."
+    exit 1
+fi
 
 unset $(env|awk -F= '/^(PMI|SLURM)_/ {if ($1 != "SLURM_ACCOUNT") print $1}')
 
