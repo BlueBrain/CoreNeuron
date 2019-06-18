cdc699b09d1eef8c4aefe1f43f9e28defd574a73
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-06-18 15:05:22 +0200

    Fix for selecting the first version of neuron with `/parallel` found

--- tests/jenkins/nrnivmodl.sh
+++ tests/jenkins/nrnivmodl.sh
@@ -7,7 +7,7 @@ TEST_DIR="$1"
 
 . /gpfs/bbp.cscs.ch/apps/hpc/jenkins/config/modules.sh
 module load intel
-module load `module av 2>&1 | grep -o 'neuron.*/parallel'`
+module load `module av neuron 2>&1 | grep -o -m 1 'neuron.*/parallel' | awk -F' ' '{print $1}'`
 
 unset $(env|awk -F= '/^(PMI|SLURM)_/ {if ($1 != "SLURM_ACCOUNT") print $1}')
 
