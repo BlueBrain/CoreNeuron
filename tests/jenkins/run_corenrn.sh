bfc46b481e4eb2fff8f1f5700bb1fa8545cc5d5c
Antonio Bellotta <antonio.bellotta@epfl.ch>
2019-06-19 17:22:42 +0200

    Added `--show` option to facilitate debugging.

--- tests/jenkins/run_corenrn.sh
+++ tests/jenkins/run_corenrn.sh
@@ -17,11 +17,11 @@ else
 fi
 
 if [ "${TEST}" = "patstim" ]; then
-    mpirun -n ${MPI_RANKS} ./${CORENRN_TYPE}/special-core --mpi -e 100 input --pattern patstim.spk -d test${TEST}dat output -o ${TEST}
+    mpirun -n ${MPI_RANKS} ./${CORENRN_TYPE}/special-core --show --mpi -e 100 input --pattern patstim.spk -d test${TEST}dat output -o ${TEST}
 elif [ "${TEST}" = "ringtest" ]; then
-    mpirun -n ${MPI_RANKS} ./${CORENRN_TYPE}/special-core --mpi -e 100 input -d coredat output -o ${TEST}
+    mpirun -n ${MPI_RANKS} ./${CORENRN_TYPE}/special-core --show --mpi -e 100 input -d coredat output -o ${TEST}
 else
-    mpirun -n ${MPI_RANKS} ./${CORENRN_TYPE}/special-core --mpi -e 100 input -d test${TEST}dat output -o ${TEST}
+    mpirun -n ${MPI_RANKS} ./${CORENRN_TYPE}/special-core --show --mpi -e 100 input -d test${TEST}dat output -o ${TEST}
 fi
 
 cat ${TEST}/out.dat > ${TEST}/out_cn_${TEST}.spk
