5c1f50a3b0da1794ce3a6a0f1fae96769ef4a29c
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2019-10-05 09:29:23 +0200

    Support for find_package   - add cmake configs for coreneuron-config.cmake   - export targets   - create libcorenrnmech.so similar to neuron under <arch>/.libs/   - change share/coreneuron/nrnivmodl_core_makefile to bin/nrnivmodl_core_makefile   - do --init --recursive automatically for submodule

--- CMake/config/DeclareMod2CSubModule.cmake
+++ CMake/config/DeclareMod2CSubModule.cmake
@@ -30,9 +30,16 @@ find_path(MOD2C_PROJ NAMES CMakeLists.txt PATHS "${CORENEURON_PROJECT_SOURCE_DIR
 find_package_handle_standard_args(MOD2C REQUIRED_VARS MOD2C_PROJ)
 
 if (NOT MOD2C_FOUND)
-  message (FATAL_ERROR "missing mod2c submodule :run on top directory of your sources (git > 1.8.2 required):
-      git submodule update --init --remote
-      ")
+  find_package(Git 1.8.3 QUIET)
+  if(NOT ${GIT_FOUND})
+    message(FATAL_ERROR "git not found, clone repository with --recursive")
+  endif()
+  message (STATUS "Sub-project : mod2c is missing : running git submodule update --init --recursive")
+  execute_process(COMMAND
+    ${GIT_EXECUTABLE} submodule update --init --recursive -- ${CORENEURON_PROJECT_SOURCE_DIR}/external/mod2c
+    WORKING_DIRECTORY ${CORENEURON_PROJECT_SOURCE_DIR})
+else()
+  message(STATUS "Sub-project : using mod2c from from ${MOD2C_PROJ}")
 endif()
 
 set(ExternalProjectCMakeArgs
