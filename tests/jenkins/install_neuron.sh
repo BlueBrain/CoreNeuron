3221d1898194ad3581aea5df298df4f2b9171f5e
Nicolas Cornu <nicolas.cornu@epfl.ch>
2020-01-16 13:38:59 +0100

    jenkins: add a parameter to build a specific branch of CoreNeuron

--- tests/jenkins/install_neuron.sh
+++ tests/jenkins/install_neuron.sh
@@ -8,8 +8,12 @@ source $WORKSPACE/BUILD_HOME/spack/share/spack/setup-env.sh
 source /gpfs/bbp.cscs.ch/apps/hpc/jenkins/config/modules.sh
 export PATH=$WORKSPACE/BUILD_HOME/spack/bin:/usr/bin:$PATH
 export MODULEPATH=$SPACK_INSTALL_PREFIX/modules/tcl/$(spack arch):$MODULEPATH
+export CORENEURON_REPO=https://github.com/BlueBrain/CoreNeuron.git
+export CORENEURON_BRANCH=${CORENEURON_BRANCH:-"master"}
+export CORENEURON_ROOT=${BUILD_HOME}/CoreNeuron
 
 unset $(env|awk -F= '/^(PMI|SLURM)_/ {if ($1 != "SLURM_ACCOUNT") print $1}')
 
-spack install neuron@develop
+git clone $CORENEURON_REPO $CORENEURON_ROOT --depth 1 -b $CORENEURON_BRANCH
+spack diy neuron@develop
 module av neuron
