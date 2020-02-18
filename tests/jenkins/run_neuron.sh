d84747aec82b3b69ec226abce11a0585608808ac
Nicolas Cornu <nicolas.cornu@epfl.ch>
2020-02-18 18:18:45 +0100

    Neuron takes -mpi and not --mpi

--- tests/jenkins/run_neuron.sh
+++ tests/jenkins/run_neuron.sh
@@ -13,12 +13,12 @@ cd $WORKSPACE/${TEST_DIR}
 if [ "${TEST_DIR}" = "testcorenrn" ]; then
     mkdir test${TEST}dat
     mkdir ${TEST}
-    mpirun -n ${MPI_RANKS} ./x86_64/special --mpi -c sim_time=100 test${TEST}.hoc
+    mpirun -n ${MPI_RANKS} ./x86_64/special -mpi -c sim_time=100 test${TEST}.hoc
     cat out${TEST}.dat | sort -k 1n,1n -k 2n,2n > ${TEST}/out_nrn_${TEST}.spk
     rm out${TEST}.dat
 elif [ "${TEST_DIR}" = "ringtest" ]; then
     mkdir ${TEST}
-    mpirun -n 6 ./x86_64/special ringtest.py --mpi
+    mpirun -n 6 ./x86_64/special ringtest.py -mpi
     cat coredat/spk1.std | sort -k 1n,1n -k 2n,2n > ${TEST}/out_nrn_${TEST}.spk
 else
     echo "Not a valid TEST"
