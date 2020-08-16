072ff8c10646fc115a0c008bc2520417341b4bb1
Michael Hines <michael.hines@yale.edu>
2020-08-16 18:08:15 -0400

    Two allocation methods in use for int arrays in InterleaveInfo.

--- coreneuron/permute/cellorder.hpp
+++ coreneuron/permute/cellorder.hpp
@@ -1,6 +1,7 @@
 #ifndef cellorder_h
 #define cellorder_h
 
+#include "coreneuron/utils/memory.h"
 #include <algorithm>
 namespace coreneuron {
 int* interleave_order(int ith, int ncell, int nnode, int* parent);
@@ -50,6 +51,13 @@ int* node_order(int ncell,
 template <typename T>
 void copy_array(T*& dest, T* src, size_t n) {
     dest = new T[n];
+    std::copy(src, src + n, dest);
+}
+
+// copy src array to dest with NRN_SOA_BYTE_ALIGN ecalloc_align allocation
+template <typename T>
+void copy_align_array(T*& dest, T* src, size_t n) {
+    dest = (T*)ecalloc_align(n, sizeof(T));
     std::copy(src, src + n, dest);
 }
 
