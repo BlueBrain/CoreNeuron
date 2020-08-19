ab38113cb04ce380b5d9e8d55e521f1797510826
Michael Hines <michael.hines@yale.edu>
2020-08-19 10:34:24 -0400

    Data return for mechanisms.

--- coreneuron/io/core2nrn_data_return.cpp
+++ coreneuron/io/core2nrn_data_return.cpp
@@ -38,7 +38,42 @@ size_t (*nrn2core_type_return_)(int type, int tid,
 namespace coreneuron {
 
 static void inverse_permute_copy(size_t n, double* permuted_src, double* dest,
-                                 int* permute);
+                                 int* permute)
+{
+  if (permute) {
+    for (size_t i = 0; i < n; ++i) {
+      dest[permute[i]] = permuted_src[i];
+    }
+  }else{
+    std::copy(permuted_src, permuted_src + n, dest);
+  }
+}
+
+static void soa2aos_inverse_permute_copy(size_t n, int sz, int stride,
+  double* src, double** dest, int* permute)
+{
+  // src is soa and permuted. dest is n pointers to sz doubles (aos).
+  for (size_t instance = 0; instance < n; ++instance) {
+    double* d = dest[permute[instance]];
+    double* s = src + instance;
+    for (int i = 0; i < sz; ++i) {
+      d[i] = s[i*stride];
+    }
+  }
+}
+
+static void soa2aos_inverse_copy(size_t n, int sz, int stride,
+  double* src, double** dest)
+{
+  // src is soa and permuted. dest is n pointers to sz doubles (aos).
+  for (size_t instance = 0; instance < n; ++instance) {
+    double* d = dest[instance];
+    double* s = src + instance;
+    for (int i = 0; i < sz; ++i) {
+      d[i] = s[i*stride];
+    }
+  }
+}
 
 void core2nrn_data_return() {
   if (!nrn2core_type_return_) {
@@ -49,6 +84,7 @@ void core2nrn_data_return() {
     double* data = nullptr;
     double** mdata = nullptr;
     NrnThread& nt = nrn_threads[tid];
+
     if (nt.end) { // transfer voltage and possibly i_membrane_
       n = (*nrn2core_type_return_)(voltage, tid, data, mdata);
       assert(n == size_t(nt.end) && data);
@@ -60,12 +96,36 @@ void core2nrn_data_return() {
         inverse_permute_copy(n, nt.nrn_fast_imem->nrn_sav_rhs, data, nt._permute);
       }
     }
-  }
-}
 
-void inverse_permute_copy(size_t n, double* permuted_src, double* dest, int* permute) {
-  for (size_t i = 0; i < n; ++i) {
-    dest[permute[i]] = permuted_src[i];
+    for (NrnThreadMembList* tml = nt.tml; tml; tml = tml->next) {
+      int mtype = tml->index;
+      Memb_list* ml = tml->ml;
+      n = (*nrn2core_type_return_)(mtype, tid, data, mdata);
+      assert(n == size_t(ml->nodecount) && mdata);
+      if (n == 0) {
+        continue;
+      }
+      // NEURON is AoS, CoreNEURON may be SoA and may be permuted.
+      // On the NEURON side, the data is actually contiguous because of
+      // cache_efficient, but that may not be the case for ARTIFICIAL_CELL.
+      // For initial implementation simplicity, use the mdata info which gives
+      // a double* for each param_size mech instance.
+      int* permute = ml->_permute;
+      double* cndat = ml->data;
+      int layout = corenrn.get_mech_data_layout()[mtype];
+      int sz = corenrn.get_prop_param_size()[mtype];
+      if (layout == 0) { /* SoA */
+        int stride = ml->_nodecount_padded;
+        if (permute) {
+          soa2aos_inverse_permute_copy(n, sz, stride, cndat, mdata, permute);
+        } else {
+          soa2aos_inverse_copy(n, sz, stride, cndat, mdata);
+        }
+      } else { /* AoS */
+        printf("%s is AoS\n", corenrn.get_memb_func(mtype).sym);
+        assert(0);
+      }
+    }
   }
 }
 
