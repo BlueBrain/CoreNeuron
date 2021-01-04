9396cf345d36c438686528129b076d69bf7635e6
Omar Awile <omar.awile@epfl.ch>
2021-01-04 19:27:49 +0100

    mech counting now also works without MPI
    minor improvements based on @alexsavulescu suggestions

--- coreneuron/io/mech_report.cpp
+++ coreneuron/io/mech_report.cpp
@@ -9,22 +9,21 @@ namespace coreneuron {
 
 /** display global mechanism count */
 void write_mech_report() {
-#if NRNMPI
     /// mechanim count across all gids, local to rank
-    auto n_memb_func = corenrn.get_memb_funcs().size();
+    const auto n_memb_func = corenrn.get_memb_funcs().size();
     std::vector<unsigned long long> local_mech_count(n_memb_func, 0);
 
     /// each gid record goes on separate row, only check non-empty threads
     for (size_t i = 0; i < nrn_nthread; i++) {
         const auto& nt = nrn_threads[i];
-        std::vector<int> mech_count(n_memb_func, 0);
         for (auto* tml = nt.tml; tml; tml = tml->next) {
-            int type = tml->index;
+            const int type = tml->index;
             const auto& ml = tml->ml;
             local_mech_count[type] += ml->nodecount;
         }
     }
 
+#if NRNMPI
     /// get global sum of all mechanism instances
     std::vector<unsigned long long> total_mech_count(n_memb_func);
     MPI_Allreduce(&local_mech_count[0],
@@ -38,13 +37,18 @@ void write_mech_report() {
     if (nrnmpi_myid == 0) {
         printf("\n================ MECHANISMS COUNT BY TYPE ==================\n");
         printf("%4s %20s %10s\n", "Id", "Name", "Count");
-        for (int i = 0; i < total_mech_count.size(); i++) {
+        for (size_t i = 0; i < total_mech_count.size(); i++) {
             printf("%4d %20s %10lld\n", i, nrn_get_mechname(i), total_mech_count[i]);
         }
         printf("=============================================================\n");
     }
 #else
-    std::cout << "Build and run with MPI to get mechanism stats \n";
+    printf("\n================ MECHANISMS COUNT BY TYPE ==================\n");
+    printf("%4s %20s %10s\n", "Id", "Name", "Count");
+    for (size_t i = 0; i < local_mech_count.size(); i++) {
+        printf("%4d %20s %10lld\n", i, nrn_get_mechname(i), local_mech_count[i]);
+    }
+    printf("=============================================================\n");
 #endif
 }
 
