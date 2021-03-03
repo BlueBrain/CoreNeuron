BlueBrain/CoreNeuron

commit f0acbdaf93c48fe8cba70b974ce56918c8d90410

Refactoring of nrn_setup.cpp in separate phases (phase1.cpp / phase2.cpp) (#283)
Major refactoring of monolithic nrn_setup.cpp into two separate phases that allows to
re-use the code for file based transfer as well as memory transfer:
 * Rewrite phase1, phase2 and move into separate .cpp files
 * Remove global static user variables
 * Add OMP_Mutex wrapper for handling OpenMP mutex
 * VecPlay now use std::vector instead of raw pointers
     - Give ownership of y_, t_ to VecPlayContinuous
 * Remove lock / unlock from fixed_vector as it was not used 
 * Remove things related to endianess : code as well as tests
 * Use name of phase instead of numbers with template arguments
* Check mechanism compatibility before reading data
   - if mechanism / mod file is different, we can't read data
     because sizes between neuron and coreneuron will be different.
     This will result into segfault / memory error.
  - check mechanism compatibility right after mechanism information
     is available

Co-authored-by: Omar Awile <omar.awile@epfl.ch>
Co-authored-by: pramodk <pramod.s.kumbhar@gmail.com>

Thanks @alkino for major work!

coreneuron/gpu/nrn_acc_manager.cpp
@@ -22,7 +22,7 @@
 #endif
 namespace coreneuron {
 extern InterleaveInfo* interleave_info;
-void copy_ivoc_vect_to_device(IvocVect*& iv, IvocVect*& div);
+void copy_ivoc_vect_to_device(const IvocVect& iv, IvocVect& div);
 void nrn_ion_global_map_copyto_device();
 void nrn_VecPlay_copyto_device(NrnThread* nt, void** d_vecplay);
 
@@ -389,21 +389,19 @@ void setup_nrnthreads_on_device(NrnThread* threads, int nthreads) {
 #endif
 }
 
-void copy_ivoc_vect_to_device(IvocVect*& iv, IvocVect*& div) {
+void copy_ivoc_vect_to_device(const IvocVect& from, IvocVect& to) {
 #ifdef _OPENACC
-    if (iv) {
-        IvocVect* d_iv = (IvocVect*)acc_copyin(iv, sizeof(IvocVect));
-        acc_memcpy_to_device(&div, &d_iv, sizeof(IvocVect*));
+    IvocVect* d_iv = (IvocVect*)acc_copyin((void*)&from, sizeof(IvocVect));
+    acc_memcpy_to_device(&to, d_iv, sizeof(IvocVect));
 
-        size_t n = iv->size();
-        if (n) {
-            double* d_data = (double*)acc_copyin(iv->data(), sizeof(double) * n);
-            acc_memcpy_to_device(&(d_iv->data_), &d_data, sizeof(double*));
-        }
+    size_t n = from.size();
+    if (n) {
+        double* d_data = (double*)acc_copyin((void*)from.data(), sizeof(double) * n);
+        acc_memcpy_to_device(&(d_iv->data_), &d_data, sizeof(double*));
     }
 #else
-    (void)iv;
-    (void)div;
+    (void)from;
+    (void)to;
 #endif
 }
 
@@ -1004,8 +1002,10 @@ void nrn_VecPlay_copyto_device(NrnThread* nt, void** d_vecplay) {
         /** copy y_, t_ and discon_indices_ */
         copy_ivoc_vect_to_device(vecplay_instance->y_, d_vecplay_instance->y_);
         copy_ivoc_vect_to_device(vecplay_instance->t_, d_vecplay_instance->t_);
-        copy_ivoc_vect_to_device(vecplay_instance->discon_indices_,
-                                 d_vecplay_instance->discon_indices_);
+        if (vecplay_instance->discon_indices_) {
+            copy_ivoc_vect_to_device(*(vecplay_instance->discon_indices_),
+                                 *(d_vecplay_instance->discon_indices_));
+         }
 
         /** copy PlayRecordEvent : todo: verify this */
         PlayRecordEvent* d_e_ =

CMake/ClangFormatHelper.cmake
@@ -23,12 +23,6 @@ if(CLANG_FORMAT_FOUND)
                     ${CORENEURON_PROJECT_SOURCE_DIR}/coreneuron/*.h*
                     ${CORENEURON_PROJECT_SOURCE_DIR}/coreneuron/*.ipp*)
 
-  # exclude ezoption headers
-  list(REMOVE_ITEM SRC_FILES_FOR_CLANG_FORMAT
-                   ${CORENEURON_PROJECT_SOURCE_DIR}/coreneuron/utils/ezoption/ezOptionParser.hpp
-                   ${CORENEURON_PROJECT_SOURCE_DIR}/coreneuron/utils/endianness.h
-                   ${CORENEURON_PROJECT_SOURCE_DIR}/coreneuron/utils/swap_endian.h)
-
   # exclude random123
   file(GLOB_RECURSE RANDOM123_FILES
                     ${CORENEURON_PROJECT_SOURCE_DIR}/coreneuron/utils/randoms/Random123/*.cpp

coreneuron/apps/main1.cpp
@@ -603,7 +603,7 @@ extern "C" int run_solve_core(int argc, char** argv) {
 
     {
         Instrumentor::phase p("checkpoint");
-        write_checkpoint(nrn_threads, nrn_nthread, corenrn_param.checkpointpath.c_str(), nrn_need_byteswap);
+        write_checkpoint(nrn_threads, nrn_nthread, corenrn_param.checkpointpath.c_str());
     }
 
     // must be done after checkpoint (to avoid deleting events)

coreneuron/io/mem_layout_util.cpp
@@ -1,0 +1,56 @@
+#include "mem_layout_util.hpp"
+
+namespace coreneuron {
+
+/// calculate size after padding for specific memory layout
+// Warning: this function is declared extern in nrniv_decl.h
+int nrn_soa_padded_size(int cnt, int layout) {
+    return soa_padded_size<NRN_SOA_PAD>(cnt, layout);
+}
+
+/// return the new offset considering the byte aligment settings
+size_t nrn_soa_byte_align(size_t size) {
+    if (LAYOUT == Layout::SoA) {
+        size_t dbl_align = NRN_SOA_BYTE_ALIGN / sizeof(double);
+        size_t remainder = size % dbl_align;
+        if (remainder) {
+            size += dbl_align - remainder;
+        }
+        nrn_assert((size * sizeof(double)) % NRN_SOA_BYTE_ALIGN == 0);
+    }
+    return size;
+}
+
+int nrn_i_layout(int icnt, int cnt, int isz, int sz, int layout) {
+    switch(layout) {
+      case Layout::AoS:
+        return icnt * sz + isz;
+      case Layout::SoA:
+        int padded_cnt = nrn_soa_padded_size(cnt, layout);  // may want to factor out to save time
+        return icnt + isz * padded_cnt;
+    }
+
+    nrn_assert(false);
+    return 0;
+}
+
+// file data is AoS. ie.
+// organized as cnt array instances of mtype each of size sz.
+// So input index i refers to i_instance*sz + i_item offset
+// Return the corresponding SoA index -- taking into account the
+// alignment requirements. Ie. i_instance + i_item*align_cnt.
+
+int nrn_param_layout(int i, int mtype, Memb_list* ml) {
+    int layout = corenrn.get_mech_data_layout()[mtype];
+    switch(layout) {
+      case Layout::AoS:
+        return i;
+      case Layout::SoA:
+        nrn_assert(layout == Layout::SoA);
+        int sz = corenrn.get_prop_param_size()[mtype];
+        return nrn_i_layout(i / sz, ml->nodecount, i % sz, sz, layout);
+    }
+    nrn_assert(false);
+    return 0;
+}
+}  // namespace coreneuron

coreneuron/io/mem_layout_util.hpp
@@ -1,0 +1,45 @@
+#pragma once
+
+#include "coreneuron/coreneuron.hpp"
+#include "coreneuron/nrniv/nrniv_decl.h"
+
+namespace coreneuron {
+
+#if !defined(NRN_SOA_PAD)
+// for layout 0, every range variable array must have a size which
+// is a multiple of NRN_SOA_PAD doubles
+#define NRN_SOA_PAD 8
+#endif
+
+// If MATRIX_LAYOUT is 1 then a,b,d,rhs,v,area is not padded using NRN_SOA_PAD
+// When MATRIX_LAYOUT is 0 then mechanism pdata index values into _actual_v
+// and _actual_area data need to be updated.
+enum Layout {
+    SoA = 0,
+    AoS = 1
+};
+
+#if !defined(LAYOUT)
+#define LAYOUT Layout::AoS
+#define MATRIX_LAYOUT Layout::AoS
+#else
+#define MATRIX_LAYOUT LAYOUT
+#endif
+
+/// return the new offset considering the byte aligment settings
+size_t nrn_soa_byte_align(size_t i); 
+
+/// This function return the index in a flat array of a matrix coordinate (icnt, isz).
+/// The matrix size is (cnt, sz)
+/// Depending of the layout some padding can be calculated
+int nrn_i_layout(int icnt, int cnt, int isz, int sz, int layout);
+
+// file data is AoS. ie.
+// organized as cnt array instances of mtype each of size sz.
+// So input index i refers to i_instance*sz + i_item offset
+// Return the corresponding SoA index -- taking into account the
+// alignment requirements. Ie. i_instance + i_item*align_cnt.
+
+int nrn_param_layout(int i, int mtype, Memb_list* ml);
+}  // namespace coreneuron
+

coreneuron/io/nrn_checkpoint.cpp
@@ -28,6 +28,7 @@ THE POSSIBILITY OF SUCH DAMAGE.
 #include <iostream>
 #include <sstream>
 #include <cassert>
+#include <memory>
 
 #include "coreneuron/sim/multicore.hpp"
 #include "coreneuron/nrniv/nrniv_decl.h"
@@ -60,11 +61,11 @@ class FileHandlerWrap {
     std::fstream G;
     FileHandlerWrap(){};
 
-    void open(const char* filename, bool reorder, std::ios::openmode mode = std::ios::in) {
-        F.open(filename, reorder, mode);
+    void open(const std::string& filename, std::ios::openmode mode = std::ios::in) {
+        F.open(filename, mode);
         std::ostringstream fname;
         fname << filename << ".txt";
-        G.open(fname.str().c_str(), mode);
+        G.open(fname.str(), mode);
     }
 
     void close() {
@@ -143,13 +144,12 @@ int patstimtype;
 
 // output directory to for checkpoint
 static const char* output_dir;
-static bool swap_bytes;
 
 static void write_phase2(NrnThread& nt, FileHandlerWrap& file_handle);
 static void write_tqueue(NrnThread& nt, FileHandlerWrap& file_handle);
 static void write_time(const char* dir);
 
-void write_checkpoint(NrnThread* nt, int nb_threads, const char* dir, bool swap_bytes_order) {
+void write_checkpoint(NrnThread* nt, int nb_threads, const char* dir) {
     // empty directory means the option is not enabled
     if (!strlen(dir)) {
         return;
@@ -162,7 +162,6 @@ void write_checkpoint(NrnThread* nt, int nb_threads, const char* dir, bool swap_
 #if NRNMPI
     nrnmpi_barrier();
 #endif
-    swap_bytes = swap_bytes_order;
 
     /**
      * if openmp threading needed:
@@ -189,7 +188,7 @@ static void write_phase2(NrnThread& nt, FileHandlerWrap& fh) {
     NrnThreadChkpnt& ntc = nrnthread_chkpnt[nt.id];
     filename << output_dir << "/" << ntc.file_id << "_2.dat";
 
-    fh.open(filename.str().c_str(), swap_bytes, std::ios::out);
+    fh.open(filename.str(), std::ios::out);
     fh.checkpoint(2);
 
     int n_outputgid = 0;  // calculate PreSyn with gid >= 0
@@ -580,10 +579,10 @@ static void write_phase2(NrnThread& nt, FileHandlerWrap& fh) {
 #endif
         if (vtype == VecPlayContinuousType) {
             VecPlayContinuous* vpc = (VecPlayContinuous*)pr;
-            int sz = vector_capacity(vpc->y_);
+            int sz = vpc->y_.size();
             fh << sz << "\n";
-            fh.write_array<double>(vector_vec(vpc->y_), sz);
-            fh.write_array<double>(vector_vec(vpc->t_), sz);
+            fh.write_array<double>(vpc->y_.data(), sz);
+            fh.write_array<double>(vpc->t_.data(), sz);
         } else {
             std::cerr << "Error checkpointing vecplay type" << std::endl;
             assert(0);
@@ -605,7 +604,7 @@ static void write_time(const char* output_dir) {
     std::ostringstream filename;
     FileHandler f;
     filename << output_dir << "/time.dat";
-    f.open(filename.str().c_str(), swap_bytes, std::ios::out);
+    f.open(filename.str(), std::ios::out);
     f.write_array(&t, 1);
     f.close();
 }
@@ -617,7 +616,7 @@ double restore_time(const char* restore_dir) {
         std::ostringstream filename;
         FileHandler f;
         filename << restore_dir << "/time.dat";
-        f.open(filename.str().c_str(), swap_bytes, std::ios::in);
+        f.open(filename.str(), std::ios::in);
         f.read_array(&rtime, 1);
         f.close();
     }
@@ -698,48 +697,40 @@ static void write_tqueue(TQItem* q, NrnThread& nt, FileHandlerWrap& fh) {
 static int patstim_index;
 static double patstim_te;
 
-static void checkpoint_restore_tqitem(int type, NrnThread& nt, FileHandler& fh) {
-    double te;
-    fh.read_array(&te, 1);
-    // printf("restore tqitem type=%d te=%.20g\n", type, te);
+static void checkpoint_restore_tqitem(int type, std::shared_ptr<Phase2::EventTypeBase> event, NrnThread& nt) {
+    // printf("restore tqitem type=%d time=%.20g\n", type, time);
 
     switch (type) {
         case NetConType: {
-            int ncindex = fh.read_int();
-            // printf("  NetCon %d\n", ncindex);
-            NetCon* nc = nt.netcons + ncindex;
-            nc->send(te, net_cvode_instance, &nt);
+            auto e = static_cast<Phase2::NetConType_*>(event.get());
+            // printf("  NetCon %d\n", netcon_index);
+            NetCon* nc = nt.netcons + e->netcon_index;
+            nc->send(e->time, net_cvode_instance, &nt);
             break;
         }
         case SelfEventType: {
-            int target_type = fh.read_int();  // not really needed (except for assert below)
-            int pinstance = fh.read_int();
-            int target_instance = fh.read_int();
-            double flag;
-            fh.read_array(&flag, 1);
-            int movable = fh.read_int();
-            int weight_index = fh.read_int();
-            if (target_type == patstimtype) {
+            auto e = static_cast<Phase2::SelfEventType_*>(event.get());
+            if (e->target_type == patstimtype) {
                 if (nt.id == 0) {
-                    patstim_te = te;
+                    patstim_te = e->time;
                 }
                 break;
             }
-            Point_process* pnt = nt.pntprocs + pinstance;
-            // printf("  SelfEvent %d %d %d %g %d %d\n", target_type, pinstance, target_instance,
+            Point_process* pnt = nt.pntprocs + e->point_proc_instance;
+            // printf("  SelfEvent %d %d %d %g %d %d\n", target_type, point_proc_instance, target_instance,
             // flag, movable, weight_index);
-            assert(target_instance == pnt->_i_instance);
-            assert(target_type == pnt->_type);
-            net_send(nt._vdata + movable, weight_index, pnt, te, flag);
+            nrn_assert(e->target_instance == pnt->_i_instance);
+            nrn_assert(e->target_type == pnt->_type);
+            net_send(nt._vdata + e->movable, e->weight_index, pnt, e->time, e->flag);
             break;
         }
         case PreSynType: {
-            int psindex = fh.read_int();
-            // printf("  PreSyn %d\n", psindex);
-            PreSyn* ps = nt.presyns + psindex;
+            auto e = static_cast<Phase2::PreSynType_*>(event.get());
+            // printf("  PreSyn %d\n", presyn_index);
+            PreSyn* ps = nt.presyns + e->presyn_index;
             int gid = ps->output_index_;
             ps->output_index_ = -1;
-            ps->send(te, net_cvode_instance, &nt);
+            ps->send(e->time, net_cvode_instance, &nt);
             ps->output_index_ = gid;
             break;
         }
@@ -749,13 +740,9 @@ static void checkpoint_restore_tqitem(int type, NrnThread& nt, FileHandler& fh) 
             break;
         }
         case PlayRecordEventType: {
-            int prtype = fh.read_int();
-            if (prtype == VecPlayContinuousType) {
-                VecPlayContinuous* vpc = (VecPlayContinuous*)(nt._vecplay[fh.read_int()]);
-                vpc->e_->send(te, net_cvode_instance, &nt);
-            } else {
-                assert(0);
-            }
+            auto e = static_cast<Phase2::PlayRecordEventType_*>(event.get());
+            VecPlayContinuous* vpc = (VecPlayContinuous*)(nt._vecplay[e->vecplay_index]);
+            vpc->e_->send(e->time, net_cvode_instance, &nt);
             break;
         }
         default: {
@@ -817,38 +804,50 @@ static void write_tqueue(NrnThread& nt, FileHandlerWrap& fh) {
 
 static bool checkpoint_restored_ = false;
 
-void checkpoint_restore_tqueue(NrnThread& nt, FileHandler& fh) {
+// Read a tqueue/checkpoint
+// int :: should be equal to the previous n_vecplay
+// n_vecplay:
+//   int: last_index
+//   int: discon_index
+//   int: ubound_index
+// int: patstim_index
+// int: should be -1
+// n_presyn:
+//   int: flags of presyn_helper
+// int: should be -1
+// null terminated:
+//   int: type
+//   array of size 1:
+//     double: time
+//   ... depends of the type
+// int: should be -1
+// null terminated:
+//   int: TO BE DEFINED
+//   ... depends of the type
+void checkpoint_restore_tqueue(NrnThread& nt, const Phase2& p2) {
     int type;
     checkpoint_restored_ = true;
 
-    // VecPlayContinuous
-    assert(fh.read_int() == nt.n_vecplay);  // VecPlayContinuous state
     for (int i = 0; i < nt.n_vecplay; ++i) {
         VecPlayContinuous* vpc = (VecPlayContinuous*)nt._vecplay[i];
-        vpc->last_index_ = fh.read_int();
-        vpc->discon_index_ = fh.read_int();
-        vpc->ubound_index_ = fh.read_int();
+        auto& vec = p2.vec_play_continuous[i];
+        vpc->last_index_ = vec.last_index;
+        vpc->discon_index_ = vec.discon_index;
+        vpc->ubound_index_ = vec.ubound_index;
     }
 
     // PatternStim
-    patstim_index = fh.read_int();  // PatternStim
+    patstim_index = p2.patstim_index;  // PatternStim
     if (nt.id == 0) {
         patstim_te = -1.0;  // changed if relevant SelfEvent;
     }
 
-    assert(fh.read_int() == -1);  // -1 PreSyn ConditionEvent flags
     for (int i = 0; i < nt.n_presyn; ++i) {
-        nt.presyns_helper[i].flag_ = fh.read_int();
+        nt.presyns_helper[i].flag_ = p2.preSynConditionEventFlags[i];
     }
 
-    assert(fh.read_int() == -1);  // -1 TQItems from atomic_dq
-    while ((type = fh.read_int()) != 0) {
-        checkpoint_restore_tqitem(type, nt, fh);
-    }
-
-    assert(fh.read_int() == -1);  // -1 TQItems from binq_
-    while ((type - fh.read_int()) != 0) {
-        checkpoint_restore_tqitem(type, nt, fh);
+    for (const auto& event: p2.events) {
+        checkpoint_restore_tqitem(event.first, event.second, nt);
     }
 }
 

coreneuron/io/nrn_checkpoint.hpp
@@ -28,6 +28,9 @@ THE POSSIBILITY OF SUCH DAMAGE.
 
 #ifndef _H_NRNCHECKPOINT_
 #define _H_NRNCHECKPOINT_
+
+#include "coreneuron/io/phase2.hpp"
+
 namespace coreneuron {
 class NrnThread;
 class FileHandler;
@@ -36,10 +39,9 @@ extern bool nrn_checkpoint_arg_exists;
 
 void write_checkpoint(NrnThread* nt,
                       int nb_threads,
-                      const char* dir,
-                      bool swap_bytes_order = false);
+                      const char* dir);
 
-void checkpoint_restore_tqueue(NrnThread&, FileHandler&);
+void checkpoint_restore_tqueue(NrnThread&, const Phase2& p2);
 
 int* inverse_permute(int* p, int n);
 void nrn_inverse_i_layout(int i, int& icnt, int cnt, int& isz, int sz, int layout);

coreneuron/io/nrn_filehandler.cpp
@@ -31,24 +31,23 @@ THE POSSIBILITY OF SUCH DAMAGE.
 #include "coreneuron/nrnconf.h"
 
 namespace coreneuron {
-FileHandler::FileHandler(const char* filename, bool reorder) {
-    this->open(filename, reorder);
-    checkpoint(0);
-    stored_chkpnt = 0;
+FileHandler::FileHandler(const std::string& filename)
+    : chkpnt(0), stored_chkpnt(0) {
+    this->open(filename);
 }
 
-bool FileHandler::file_exist(const char* filename) const {
+bool FileHandler::file_exist(const std::string& filename) const {
     struct stat buffer;
-    return (stat(filename, &buffer) == 0);
+    return (stat(filename.c_str(), &buffer) == 0);
 }
 
-void FileHandler::open(const char* filename, bool reorder, std::ios::openmode mode) {
+void FileHandler::open(const std::string& filename, std::ios::openmode mode) {
     nrn_assert((mode & (std::ios::in | std::ios::out)));
-    reorder_bytes = reorder;
     close();
     F.open(filename, mode | std::ios::binary);
-    if (!F.is_open())
-        fprintf(stderr, "cannot open file %s\n", filename);
+    if (!F.is_open()) {
+        std::cerr << "cannot open file '" << filename << "'" << std::endl;
+    }
     nrn_assert(F.is_open());
     current_mode = mode;
     char version[256];

commit f2e152b0892709f914088dcc61c2c1f6515eae12

Bug fix for copying VecPlay's IvocVect object on GPU  - acc_memcpy_to_device is used to copy copy the device pointer  - so we have use address of device pointer and sizeof(IvocVect*)    as the size to copy the pointer. Note that IvocVect is already    copied by previous acc_copyin call  - this issue was introduced in #283
fixes #501

coreneuron/gpu/nrn_acc_manager.cpp
@@ -402,7 +402,7 @@ void setup_nrnthreads_on_device(NrnThread* threads, int nthreads) {
 void copy_ivoc_vect_to_device(const IvocVect& from, IvocVect& to) {
 #ifdef _OPENACC
     IvocVect* d_iv = (IvocVect*) acc_copyin((void*) &from, sizeof(IvocVect));
-    acc_memcpy_to_device(&to, d_iv, sizeof(IvocVect));
+    acc_memcpy_to_device(&to, &d_iv, sizeof(IvocVect*));
 
     size_t n = from.size();
     if (n) {