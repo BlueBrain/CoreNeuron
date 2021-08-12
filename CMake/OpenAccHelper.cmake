8e6ff6b1bee6f585e6952178f2b2af1ffceeb0b1
Olli Lupton <oliver.lupton@epfl.ch>
2021-08-12 13:49:01 +0200

    CMake minimum v3.15 for GPU builds.

--- CMake/OpenAccHelper.cmake
+++ CMake/OpenAccHelper.cmake
@@ -14,15 +14,38 @@ if(CORENRN_ENABLE_GPU)
   if(CORENRN_ENABLE_CUDA_UNIFIED_MEMORY)
     add_compile_definitions(CORENEURON_UNIFIED_MEMORY)
   endif()
-  # This is a lazy way of getting the major/minor versions separately without parsing
-  # ${CMAKE_CUDA_COMPILER_VERSION}
-  find_package(CUDAToolkit 9.0 REQUIRED)
-  # Be a bit paranoid
-  if(NOT ${CMAKE_CUDA_COMPILER_VERSION} STREQUAL ${CUDAToolkit_VERSION})
-    message(
-      FATAL_ERROR
-        "CUDA compiler (${CMAKE_CUDA_COMPILER_VERSION}) and toolkit (${CUDAToolkit_VERSION}) versions are not the same!"
-    )
+  if(${CMAKE_VERSION} VERSION_LESS 3.17)
+    # Hopefully we can drop this soon. Parse ${CMAKE_CUDA_COMPILER_VERSION} into a shorter X.Y
+    # version without any patch version.
+    if(NOT ${CMAKE_CUDA_COMPILER_ID} STREQUAL "NVIDIA")
+      message(FATAL_ERROR "Unsupported CUDA compiler ${CMAKE_CUDA_COMPILER_ID}")
+    endif()
+    # Parse CMAKE_CUDA_COMPILER_VERSION=x.y.z into CUDA_VERSION=x.y
+    string(FIND ${CMAKE_CUDA_COMPILER_VERSION} . first_dot)
+    math(EXPR first_dot_plus_one "${first_dot}+1")
+    string(SUBSTRING ${CMAKE_CUDA_COMPILER_VERSION} ${first_dot_plus_one} -1 minor_and_later)
+    string(FIND ${minor_and_later} . second_dot_relative)
+    if(${first_dot} EQUAL -1 OR ${second_dot_relative} EQUAL -1)
+      message(
+        FATAL_ERROR
+          "Failed to parse a CUDA_VERSION from CMAKE_CUDA_COMPILER_VERSION=${CMAKE_CUDA_COMPILER_VERSION}"
+      )
+    endif()
+    math(EXPR second_dot_plus_one "${first_dot}+${second_dot_relative}+1")
+    string(SUBSTRING ${CMAKE_CUDA_COMPILER_VERSION} 0 ${second_dot_plus_one}
+                     CORENRN_CUDA_VERSION_SHORT)
+  else()
+    # This is a lazy way of getting the major/minor versions separately without parsing
+    # ${CMAKE_CUDA_COMPILER_VERSION}
+    find_package(CUDAToolkit 9.0 REQUIRED)
+    # Be a bit paranoid
+    if(NOT ${CMAKE_CUDA_COMPILER_VERSION} STREQUAL ${CUDAToolkit_VERSION})
+      message(
+        FATAL_ERROR
+          "CUDA compiler (${CMAKE_CUDA_COMPILER_VERSION}) and toolkit (${CUDAToolkit_VERSION}) versions are not the same!"
+      )
+    endif()
+    set(CORENRN_CUDA_VERSION_SHORT "${CUDAToolkit_VERSION_MAJOR}.${CUDAToolkit_VERSION_MINOR}")
   endif()
   # -acc enables OpenACC support, -cuda links CUDA libraries and (very importantly!) seems to be
   # required to make the NVHPC compiler do the device code linking. Otherwise the explicit CUDA
@@ -32,8 +55,7 @@ if(CORENRN_ENABLE_GPU)
   # due to e.g. __CUDACC__ being defined. See https://github.com/BlueBrain/CoreNeuron/issues/607 for
   # more information about this. -gpu=cudaX.Y ensures that OpenACC code is compiled with the same
   # CUDA version as is used for the explicit CUDA code.
-  set(NVHPC_ACC_COMP_FLAGS
-      "-acc -gpu=cuda${CUDAToolkit_VERSION_MAJOR}.${CUDAToolkit_VERSION_MINOR}")
+  set(NVHPC_ACC_COMP_FLAGS "-acc -gpu=cuda${CORENRN_CUDA_VERSION_SHORT}")
   set(NVHPC_ACC_LINK_FLAGS "-cuda")
   # Make sure that OpenACC code is generated for the same compute capabilities as the explicit CUDA
   # code. Otherwise there may be confusing linker errors. We cannot rely on nvcc and nvc++ using the
