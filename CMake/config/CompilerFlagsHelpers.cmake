12f3859e6e9a597d21d9def4cd4476d69ee008c0
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2022-07-06 21:26:25 +0200

    I hate formatting checks

--- CMake/config/CompilerFlagsHelpers.cmake
+++ CMake/config/CompilerFlagsHelpers.cmake
@@ -35,7 +35,8 @@ foreach(COMPILER_LANGUAGE ${SUPPORTED_COMPILER_LANGUAGE_LIST})
     # XLC -qinfo=all is awfully verbose on any platforms that use the GNU STL
     # Enable by default only the relevant one
     # ~~~
-    set(CMAKE_${COMPILER_LANGUAGE}_WARNING_ALL "-qformat=all -qinfo=lan:trx:ret:zea:cmp:ret")
+    set(CMAKE_${COMPILER_LANGUAGE}_WARNING_ALL
+        "-qformat=all -qinfo=lan:trx:ret:zea:cmp:ret")
 
     set(CMAKE_${COMPILER_LANGUAGE}_DEBUGINFO_FLAGS "-g")
 
@@ -89,8 +90,8 @@ foreach(COMPILER_LANGUAGE ${SUPPORTED_COMPILER_LANGUAGE_LIST})
       set(CMAKE_${COMPILER_LANGUAGE}_LINK_TIME_OPT "-flto")
     endif()
 
-    if((CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "^ppc") OR (CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "^power"
-                                                       ))
+    if((CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "^ppc")
+       OR (CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "^power"))
       # ppc arch do not support -march= syntax
       set(CMAKE_${COMPILER_LANGUAGE}_GEN_NATIVE "-mcpu=native")
     else()
@@ -110,9 +111,10 @@ foreach(COMPILER_LANGUAGE ${SUPPORTED_COMPILER_LANGUAGE_LIST})
     set(CMAKE_${COMPILER_LANGUAGE}_STACK_PROTECTION "-fstack-protector")
     set(CMAKE_${COMPILER_LANGUAGE}_POSITION_INDEPENDENT "-fPIC")
 
-    # Force same ld behavior as when called from gcc --as-needed forces the linker to check whether
-    # a dynamic library mentioned in the command line is actually needed by the objects being
-    # linked. Symbols needed in shared objects are already linked when building that library.
+    # Force same ld behavior as when called from gcc --as-needed forces the
+    # linker to check whether a dynamic library mentioned in the command line is
+    # actually needed by the objects being linked. Symbols needed in shared
+    # objects are already linked when building that library.
     set(CMAKE_EXE_LINKER_FLAGS "-Wl,--as-needed")
     set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--as-needed")
 
@@ -131,7 +133,8 @@ foreach(COMPILER_LANGUAGE ${SUPPORTED_COMPILER_LANGUAGE_LIST})
     set(CMAKE_${COMPILER_LANGUAGE}_VECTORIZE "")
 
     if(CMAKE_${COMPILER_LANGUAGE}_COMPILER_IS_ICC)
-      # unknown compiler flags produce error on Cray and hence just set this for intel now
+      # unknown compiler flags produce error on Cray and hence just set this for
+      # intel now
       set(IGNORE_UNKNOWN_PRAGMA_FLAGS "-Wno-unknown-pragmas")
       # Intel O3 is extreme
       set(CMAKE_${COMPILER_LANGUAGE}_OPT_AGGRESSIVE "-O2")
@@ -147,17 +150,21 @@ endforeach()
 # ===============================================================================
 # Allow undefined reference in shared library as mod files will be linked later
 # ===============================================================================
-if(CMAKE_CXX_COMPILER_ID MATCHES "AppleClang" OR ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+if(CMAKE_CXX_COMPILER_ID MATCHES "AppleClang" OR ${CMAKE_SYSTEM_NAME} MATCHES
+                                                 "Darwin")
   set(UNDEFINED_SYMBOLS_IGNORE_FLAG "-undefined dynamic_lookup")
-  string(APPEND CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS " ${UNDEFINED_SYMBOLS_IGNORE_FLAG}")
-  string(APPEND CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS " ${UNDEFINED_SYMBOLS_IGNORE_FLAG}")
+  string(APPEND CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS
+         " ${UNDEFINED_SYMBOLS_IGNORE_FLAG}")
+  string(APPEND CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS
+         " ${UNDEFINED_SYMBOLS_IGNORE_FLAG}")
 endif()
 
 # ===============================================================================
-# See "Note: Special instructions for GCC 8" on https://github.com/CLIUtils/CLI11
+# See "Note: Special instructions for GCC 8" on
+# https://github.com/CLIUtils/CLI11
 # ===============================================================================
-if(CMAKE_CXX_COMPILER_IS_GCC AND
-        ${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER_EQUAL 8 AND
-        ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 9 )
-    add_compile_definitions(CLI11_HAS_FILESYSTEM=0)
+if(CMAKE_CXX_COMPILER_IS_GCC
+   AND ${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER_EQUAL 8
+   AND ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 9)
+  add_compile_definitions(CLI11_HAS_FILESYSTEM=0)
 endif()
