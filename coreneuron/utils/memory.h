429fd6236d3b693405fd1ff385f05bbc9d031faa
Omar Awile <omar.awile@epfl.ch>
2022-09-16 16:57:39 +0200

    forgot to initialize fill

--- coreneuron/utils/memory.h
+++ coreneuron/utils/memory.h
@@ -176,7 +176,7 @@ class MemoryManaged {
 #include <cstdlib>
 
 inline void alloc_memory(void*& pointer, size_t num_bytes, size_t alignment) {
-    size_t fill;
+    size_t fill = 0;
     if (num_bytes % alignment != 0) {
         size_t multiple = num_bytes / alignment;
         fill = alignment * (multiple + 1) - num_bytes;
